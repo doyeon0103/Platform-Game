c177c726a180be7638a2a3259816e413c19954c7
doyeon0103 <templer010353@gmail.com>
2024-06-13 19:54:47 +0900

    0.0.2

--- Platform Game/Assets/2D Platformer/Scripts/MoveController.cs
+++ Platform Game/Assets/2D Platformer/Scripts/MoveController.cs
@@ -1,10 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using Unity.VisualScripting;
 
 public class MoveController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("플레이어 이동 및 점프")]
+    Rigidbody2D rigid;
+    CapsuleCollider2D coll;
+    Animator anim;
+    Vector3 moveDir;
+    float verticalVelocity = 0f;
+    GameObject gameObject;
+
+    [SerializeField] float jumpForce;
+    [SerializeField] float moveSpeed;
+    [SerializeField] float groundCheckLength;
+
+    [SerializeField] Color colorGroundCheck;
+    [SerializeField] bool isGround;
+    [SerializeField] bool showGroundCheck;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+    private void OnDrawGizmos()
+    {
+        if (showGroundCheck == true)
+        {
+
+        Debug.DrawLine(transform.position, transform.position - new Vector3(0, groundCheckLength), colorGroundCheck);
+        }
+    }
+
     void Start()
     {
         
@@ -13,6 +43,25 @@ public class MoveController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        checkGround();
+        moving();
+    }
+    private void checkGround()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, groundCheckLength, LayerMask.GetMask("Ground"));
+        if (hit)
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+    }
+    private void moving()
+    {
+        moveDir.x = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        moveDir.y = rigid.velocity.y;
+        rigid.velocity = moveDir;
     }
 }
